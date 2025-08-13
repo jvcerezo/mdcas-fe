0b9a183049504af095f317e6055803cdff15fd25
jvcerezo <jvcerezo@up.edu.ph>
2025-08-13 21:04:11 +0800

    setup of initial api requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,4 +4,44 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+export const get = async (url, config = {}) => {
+  try {
+    const response = await api.get(url, config);
+    return response.data;
+  } catch (error) {
+    console.error('GET request error:', error);
+    throw error;
+  }
+};
+
+export const post = async (url, data, config = {}) => {
+  try {
+    const response = await api.post(url, data, config);
+    return response.data;
+  } catch (error) {
+    console.error('POST request error:', error);
+    throw error;
+  }
+};
+
+export const put = async (url, data, config = {}) => {
+  try {
+    const response = await api.put(url, data, config);
+    return response.data;
+  } catch (error) {
+    console.error('PUT request error:', error);
+    throw error;
+  }
+};
+
+export const del = async (url, config = {}) => {
+  try {
+    const response = await api.delete(url, config);
+    return response.data;
+  } catch (error) {
+    console.error('DELETE request error:', error);
+    throw error;
+  }
+};
+
 export default api;
